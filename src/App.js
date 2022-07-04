33866ac3b11ef3318942047ae0ff521c36c9886f
arthur <aa19341@essex.ac.uk>
2022-07-04 16:35:38 +0100

    Implement Styling for Search and HTML Elements

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
 import React, { useState } from "react";
 import axios from 'axios'
 
+
 function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
 
-  //const url = `https://api.openweathermap.org/data/2.5/weather?lat=51.5072&lon=0.1276&appid=331bc59e269bdfac6214881ea98bed65`
 
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=331bc59e269bdfac6214881ea98bed65`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
 
   return (
     <div className="app">
+      <div className="search-bar">
+        <input type="text" value={location} onChange={event => setLocation(event.target.value)}
+        placeholder="Enter Location"
+        onKeyPress={searchLocation}>
+        </input>
+      </div>
       <div className="container">
         <div className="top">
+          <div className="location">
+            <p>London</p>
+          </div>
+          <div className="temp">
+            <h1>17°c</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
           <div className="bottom">
-            <div className="location">
-              <p>London</p>
+            <div className="feels">
+              <p>22°c</p>
+              <p>Feels like</p>
             </div>
-            <div className="temp">
-              <h1>17°c</h1>
+            <div className="humidity">
+              <p>57%</p>
+              <p>Humidity</p>
             </div>
-            <div className="description">
-
+            <div className="wind">
+              <p>3 MPH</p>
+              <p className="bold">Wind Speed</p>
             </div>
           </div>
         </div>
       </div>
-
-
     </div>
   );
 }
