194f7207e9ece3e2889bb162f330d3a063cfda1d
arthur <aa19341@essex.ac.uk>
2022-07-04 01:04:21 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from 'axios'
 
 function App() {
+
+  //const url = `https://api.openweathermap.org/data/2.5/weather?lat=51.5072&lon=0.1276&appid=331bc59e269bdfac6214881ea98bed65`
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="bottom">
+            <div className="location">
+              <p>London</p>
+            </div>
+            <div className="temp">
+              <h1>17°c</h1>
+            </div>
+            <div className="description">
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 }
